c5b90bb37c165bc275e3cc0f679a4b9bc527f529
renu28choudhary <renu28choudhary@gmail.com>
2023-08-18 15:00:24 -0400

    Adding event bind

--- react-pratice/src/App.js
+++ react-pratice/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Greet from './Greet';
 import Message from './Message';
 import Counter from './Counter';
+import EventBindClass from './EventBindClass';
+import ParentComponent from './ParentComponent';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Greet name="Superman"/>
       <Message />
       <Counter/>
+      <EventBindClass/>
+      <ParentComponent/>
     </div>
   );
 }
